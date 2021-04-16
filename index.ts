929ddbd7c0faebee60b1c8dd9c10115ab9446287
GrantGryczan <grantgryczan@gmail.com>
2021-04-16 02:31:08 -0400

    Update formatting

--- index.ts
+++ index.ts
@@ -6,17 +6,17 @@ export type Updater = readonly [UpdaterHook, UpdaterFunction];
 
 /**
  * Creates a [React component updater](https://github.com/GrantGryczan/react-component-updater#react-component-updater).
- * 
+ *
  * Usage:
  * ```
  * const [useMyComponentUpdater, updateMyComponent] = createUpdater();
  * ```
- * 
+ *
  * ```
  * // Call this inside any component you want to update.
  * useMyComponentUpdater();
  * ```
- * 
+ *
  * ```
  * // Call this to update any hooked components.
  * updateMyComponent();
@@ -24,28 +24,29 @@ export type Updater = readonly [UpdaterHook, UpdaterFunction];
  */
 const createUpdater = (): Updater => {
 	let currentState = false;
-	
+
 	const updateStates: Array<() => void> = [];
-	
+
 	/** Call this to update any hooked components. */
 	const update: UpdaterFunction = () => {
 		currentState = !currentState;
+
 		for (let i = 0; i < updateStates.length; i++) {
 			updateStates[i]();
 		}
 	};
-	
+
 	/** Call this inside any component you want to update. */
 	const useUpdater: UpdaterHook = () => {
 		const [, setState] = useState(currentState);
-		
+
 		useEffect(() => {
 			const updateState = () => {
 				setState(currentState);
 			};
-			
+
 			updateState._index = updateStates.push(updateState) - 1;
-			
+
 			return () => {
 				// Delete `updateState` from `updateStates` in O(1) by popping the last item and swapping it into this item's place (unless this item is the popped item).
 				const lastUpdateState = updateStates.pop() as typeof updateState;
@@ -56,7 +57,7 @@ const createUpdater = (): Updater => {
 			};
 		}, []);
 	};
-	
+
 	return [useUpdater, update] as const;
 };
 
